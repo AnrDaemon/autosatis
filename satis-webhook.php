57f77b0e5d3cc20623c92e2050cc7b8faa071350
AnrDaemon <anrdaemon@yandex.ru>
2018-03-10 12:47:17 +0300

    Use HTTP POST to call hook.

--- satis-webhook.php
+++ satis-webhook.php
@@ -1,13 +1,18 @@
 <?php
 
+if($_SERVER['REQUEST_METHOD'] !== 'POST')
+{
+  header('Location: /');
+  exit();
+}
+
 $list = json_decode(file_get_contents("{$_SERVER['DOCUMENT_ROOT']}/packages.json"), true);
-if(isset($_GET['update'], $list['providers'][$_GET['update']]))
+if(isset($_POST['update'], $list['providers'][$_POST['update']]))
 {
-  $res = file_put_contents("{$_SERVER['HOME']}/tmp/satis-update", "{$_GET['update']}\n", FILE_APPEND);
-  if(false === $res)
-    print "Ff.";
+  $res = file_put_contents("{$_SERVER['HOME']}/tmp/satis-update", "{$_POST['update']}\n", FILE_APPEND);
+  http_response_code(202);
+  exit('Operation queued.');
 }
-else
-{
-  print "Ff.";
-}
+
+http_response_code(400);
+print "Ff.";
